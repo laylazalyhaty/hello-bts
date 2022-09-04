22c30bedacba0336154dc32c1f40bd15bff133ee
Layla Zalyhaty <laylazalyhaty@gmail.com>
2022-09-04 16:45:09 +0000

    Add anything.

--- .space.kts
+++ .space.kts
@@ -4,11 +4,26 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Hello World!") {
+job("Build, push and deploy") {
     container(displayName = "Say Hello", image = "hello-world")
-}
 
-job("Hello BTS!") {
-    container(displayName = "Say Hello KNJ", image = "hello-knj")
-    container(displayName = "Say Hello KNJ", image = "hello-knj")
+	startOn {
+        gitPush {enabled = true}
+    }
+
+	container(
+         displayName = "Sending announcement to channel Deployments",
+    ) {
+    
+    kotlinScript { api -> 
+        println("Running in branch: "+ api.gitBranch())
+        api.gradlew("build")
+        
+        api.space().chats.messages.sendMessage(
+            MessageRecipient.Channel(ChatChannel.FromName("CI-Channel")),
+            ChatMessage.Text("Build has completed - build numer" + api.executionNumber()) 
+        )
+    }
 }
+}
+    
